ab8a4520474871503ebb77ab991eb24412ac8853
orj93 <136968631+orj93@users.noreply.github.com>
2023-11-08 09:39:17 +0000

    spotify exercise

--- src/app.py
+++ src/app.py
@@ -2,14 +2,31 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-# load the .env file variables
+
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+# export SPOTIPY_CLIENT_ID='8c425a2e376846f0b3d53d8d4fa9a9a0'
+# export SPOTIPY_CLIENT_SECRET='97aac6d4b18341d1b81aacb9f1fe04cd'
+# export SPOTIPY_REDIRECT_URI='http://localhost/'
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+client_id = "8c425a2e376846f0b3d53d8d4fa9a9a0"
+client_secret = "97aac6d4b18341d1b81aacb9f1fe04cd"
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+metallica_uri = 'spotify:artist:1a2b3c4d5e6f7g2ye2Wgw4gimLv2eAKyk1NB?si=T3WOLkgZSiispl0oV5TO2Q'
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id,client_secret=client_secret ))
+
+results = spotify.artist_albums(metallica_uri, album_type='album')
+
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
+
+
